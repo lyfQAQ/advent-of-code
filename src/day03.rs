53c03e06eaa0e04497ca975aedf5df85f9d69017
lyfQAQ <0219lyf@gmail.com>
2024-02-19 16:13:57 +0800

    2023 day03 part 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,71 +1,152 @@
 use std::io::BufRead;
 
-fn part_one() -> u32 {
-    let filepath = "./inputs/day03.txt";
-    let file = std::fs::File::open(filepath).unwrap();
-    let mut graph = vec![];
-    let reader = std::io::BufReader::new(file);
-    for line in reader.lines().map(|result| result.unwrap()) {
-        graph.push(line.into_bytes());
-    }
-    let n = graph.len();
-    let m = graph[0].len();
-    // println!("{:?}", graph);
-    let mut res = 0;
-    let mut i: usize = 0;
-    while i < n {
-        let line = &graph[i];
-        let mut start: usize = 0;
-        let mut j: usize = 0;
-        while !line[j].is_ascii_digit() {
-            j += 1;
-        }
-        // 数字的第一位
-        start = j;
-        while j < line.len() {
-            let mut flag = false;
-            while j < line.len() && line[j].is_ascii_digit() {
-                if check(&graph, i as i32, j as i32) {
-                    flag = true;
-                }
-                j += 1;
-            }
-            // 找到了完整数字
-            if flag {
-                let s = String::from_utf8(line[start..j].to_vec()).unwrap();
-                res += s.parse::<u32>().unwrap();
-            }
-            // 找数字的第一位
-            while j < line.len() && !line[j].is_ascii_digit() {
-                j += 1;
-            }
-            start = j;
-        }
-        i += 1;
-    }
-    res
+// fn part_one() -> u32 {
+//     let filepath = "./inputs/day03.txt";
+//     let file = std::fs::File::open(filepath).unwrap();
+//     let mut graph = vec![];
+//     let reader = std::io::BufReader::new(file);
+//     for line in reader.lines().map(|result| result.unwrap()) {
+//         graph.push(line.into_bytes());
+//     }
+//     let n = graph.len();
+//     let m = graph[0].len();
+//     // println!("{:?}", graph);
+//     let mut res = 0;
+//     let mut i: usize = 0;
+//     while i < n {
+//         let line = &graph[i];
+//         let mut start: usize = 0;
+//         let mut j: usize = 0;
+//         while !line[j].is_ascii_digit() {
+//             j += 1;
+//         }
+//         // 数字的第一位
+//         start = j;
+//         while j < line.len() {
+//             let mut flag = false;
+//             while j < line.len() && line[j].is_ascii_digit() {
+//                 if check_part_one(&graph, i as i32, j as i32) {
+//                     flag = true;
+//                 }
+//                 j += 1;
+//             }
+//             // 找到了完整数字
+//             if flag {
+//                 let s = String::from_utf8(line[start..j].to_vec()).unwrap();
+//                 res += s.parse::<u32>().unwrap();
+//             }
+//             // 找数字的第一位
+//             while j < line.len() && !line[j].is_ascii_digit() {
+//                 j += 1;
+//             }
+//             start = j;
+//         }
+//         i += 1;
+//     }
+//     res
+// }
+
+// // 检查每个数字周围是否符合条件
+// fn check_part_one(graph: &Vec<Vec<u8>>, x: i32, y: i32) -> bool {
+//     // 8 个方向
+//     let dx = [0, -1, -1, -1, 0, 1, 1, 1];
+//     let dy = [-1, -1, 0, 1, 1, 1, 0, -1];
+
+//     let n = graph.len();
+//     let m = graph[0].len();
+//     for k in 0..8 {
+//         let u: i32 = x + dx[k];
+//         let v: i32 = y + dy[k];
+//         if u < 0 || u >= n as i32 || v < 0 || v >= m as i32 {
+//             continue;
+//         }
+//         if graph[u as usize][v as usize] != b'.' && !graph[u as usize][v as usize].is_ascii_digit()
+//         {
+//             return true;
+//         }
+//     }
+//     false
+// }
+
+use std::collections::HashSet;
+#[derive(Debug)]
+struct ParNumber {
+    value: i64,
+    // 该数字能够扩展的点集
+    points: HashSet<(i64, i64)>,
 }
-
-// 检查每个数字周围是否符合条件
-fn check(graph: &Vec<Vec<u8>>, x: i32, y: i32) -> bool {
-    // 8 个方向
-    let dx = [0, -1, -1, -1, 0, 1, 1, 1];
-    let dy = [-1, -1, 0, 1, 1, 1, 0, -1];
-
-    let n = graph.len();
-    let m = graph[0].len();
-    for k in 0..8 {
-        let u: i32 = x + dx[k];
-        let v: i32 = y + dy[k];
-        if u < 0 || u >= n as i32 || v < 0 || v >= m as i32 {
-            continue;
-        }
-        if graph[u as usize][v as usize] != b'.' && !graph[u as usize][v as usize].is_ascii_digit()
-        {
-            return true;
+impl ParNumber {
+    fn new(row: i64, col: i64, ch: char) -> Self {
+        // 当前位相邻的8个方向
+        let points = HashSet::from([
+            (row - 1, col - 1),
+            (row, col - 1),
+            (row + 1, col - 1), // 左边
+            (row - 1, col),
+            (row + 1, col), // 上和下
+            (row + 1, col + 1),
+            (row, col + 1),
+            (row - 1, col + 1), // 右边
+        ]);
+        Self {
+            value: (ch as u8 - b'0') as i64,
+            points,
         }
     }
-    false
+    // 添加后续数字位
+    fn add_digit(&mut self, row: i64, col: i64, ch: char) {
+        self.value = self.value * 10 + (ch as u8 - b'0') as i64;
+        // 此时只需扩展右边的方向
+        self.points
+            .extend([(row + 1, col + 1), (row, col + 1), (row - 1, col + 1)]);
+    }
+}
+fn part_one() -> i64 {
+    let filepath = "./inputs/day03_part1.txt";
+    let file = std::fs::File::open(filepath).unwrap();
+    let reader = std::io::BufReader::new(file);
+    let lines = reader.lines().map(|result| result.unwrap());
+    let mut nums = vec![];
+    // 所有符号的位置
+    let mut syms = HashSet::new();
+    // 当前正在解析的数字序列
+    let mut cur_number: Option<ParNumber> = None;
+    for (row, line) in lines.enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut num) = cur_number {
+                    // 继续构造当前数字
+                    num.add_digit(row as i64, col as i64, ch);
+                } else {
+                    cur_number = Some(ParNumber::new(row as i64, col as i64, ch));
+                }
+            } else {
+                if let Some(num) = cur_number.take() {
+                    nums.push(num); // 当前数字解析完毕
+                }
+                if ch != '.' {
+                    // 记录符号的位置
+                    syms.insert((row as i64, col as i64));
+                }
+            }
+        }
+    }
+
+    let total = nums
+        .iter()
+        .filter(|num| num.points.intersection(&syms).next().is_some())
+        .map(|num| num.value)
+        .sum();
+
+    // for num in &nums {
+    //     // 当前数字的邻居点集合 和 所有符号的邻居点集合 求交集
+    //     if num.points.intersection(&syms).next().is_some() {
+    //         total += num.value;
+    //     }
+    // }
+
+    // println!("{:?}\n-----\n{:?}", nums, syms);s
+    total
 }
 
 #[test]
